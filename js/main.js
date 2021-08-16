e1678753399769e5bdcc1fe7a685451706af7fc8
izzatdev <toshpulatovizzatulla@gmail.com>
2021-08-16 20:09:59 +0500

    6th secton has been finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+$('.slider_box').slick({
+    prevArrow: '<img src="../images/arrow-left.svg" alt="" class="slider_arrow-prev">',
+    nextArrow: '<img src="../images/arrow-right.svg" alt="" class="slider_arrow-next">',
+});
 });
